323b529a50411c8226f21e45e850b02f7917ac3a
Matthew Odle <matt.odle@tdstelecom.com>
2017-10-06 11:36:51 -0500

    add exercise 2

--- client/js/components/car-tool.js
+++ client/js/components/car-tool.js
@@ -3,7 +3,37 @@ import * as React from 'react';
 export class CarTool extends React.Component {
 
   render() {
-    return <h1>Car Tool</h1>;
+    return <div>
+      <header>
+        <h1>Color Tool</h1>
+      </header>
+      <table border="1px">
+        <tr>
+          <th>id</th>
+          <th>Make</th>
+          <th>Model</th>
+          <th>Year</th>
+          <th>Color</th>
+          <th>Price</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Chevy</td>
+          <td>Cavalier</td>
+          <td>1981</td>
+          <td>Brown with grey doors</td>
+          <td>100</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Pontiac</td>
+          <td>Grand Prix</td>
+          <td>1990</td>
+          <td>Rusted-out blue</td>
+          <td>300</td>
+        </tr>
+      </table >
+    </div >;
   }
 
 }
