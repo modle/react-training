3e937ca976f48e7a0bb0de5f458ff5daa2bae2ef
Matthew Odle <matt.odle@tdstelecom.com>
2017-10-06 11:52:12 -0500

    add thead and tbody

--- client/js/components/car-tool.js
+++ client/js/components/car-tool.js
@@ -8,30 +8,34 @@ export class CarTool extends React.Component {
         <h1>Color Tool</h1>
       </header>
       <table border="1px">
-        <tr>
-          <th>id</th>
-          <th>Make</th>
-          <th>Model</th>
-          <th>Year</th>
-          <th>Color</th>
-          <th>Price</th>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Chevy</td>
-          <td>Cavalier</td>
-          <td>1981</td>
-          <td>Brown with grey doors</td>
-          <td>100</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Pontiac</td>
-          <td>Grand Prix</td>
-          <td>1990</td>
-          <td>Rusted-out blue</td>
-          <td>300</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Make</th>
+            <th>Model</th>
+            <th>Year</th>
+            <th>Color</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Chevy</td>
+            <td>Cavalier</td>
+            <td>1981</td>
+            <td>Brown</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Lincoln</td>
+            <td>Futura</td>
+            <td>1955</td>
+            <td>Black</td>
+            <td>NaN</td>
+          </tr>
+        </tbody>
       </table >
     </div >;
   }
