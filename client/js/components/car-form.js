a9892efaef027779792c2e72b48436e13bbf7de6
Matthew Odle <matt.odle@tdstelecom.com>
2017-10-09 07:06:33 -0500

    add car form

--- client/js/components/car-form.js
+++ client/js/components/car-form.js
@@ -1,9 +1,10 @@
 import * as React from 'react';
 
-export class ColorForm extends React.Component {
+export class CarForm extends React.Component {
   
   constructor(props) {
     super(props);
+
     this.state = {
       newMake: '',
       newModel: '',
@@ -20,31 +21,58 @@ export class ColorForm extends React.Component {
     });
   }
 
-  <form>
-        <div>
-          <label htmlFor="new-car-make-input">Make:</label>
-          <input type="text" id="new-car-make-input" name="newCarMake"
-            value={this.state.newCarMake} onChange={this.onChange} />
-        </div >
-        <div>
-          <label htmlFor="new-car-model-input">Model:</label>
-          <input type="text" id="new-car-model-input" name="newCarModel"
-            value={this.state.newCarModel} onChange={this.onChange} />
-        </div >
-        <div>
-          <label htmlFor="new-car-year-input">Year:</label>
-          <input type="number" id="new-car-year-input" name="newCarYear"
-            value={this.state.newCarYear} onChange={this.onChange} />
-        </div >
-        <div>
-          <label htmlFor="new-car-color-input">Color:</label>
-          <input type="text" id="new-car-color-input" name="newCarColor"
-            value={this.state.newCarColor} onChange={this.onChange} />
-        </div >
-        <div>
-          <label htmlFor="new-car-price-input">Price:</label>
-          <input type="number" id="new-car-price-input" name="newCarPrice"
-            value={this.state.newCarPrice} onChange={this.onChange} />
-        </div >
-        <button type="button" onClick={this.onClick}>Add Car</button>
-      </form>
+  save = () => {
+    const nextId = Math.max(...this.props.cars.map(c => c.id)) + 1;
+
+    const car = {
+      id: nextId,
+      make: this.state.newCarMake,
+      model: this.state.newCarModel,
+      year: this.state.newCarYear,
+      color: this.state.newCarColor,
+      price: this.state.newCarPrice,
+    };
+
+    this.props.onSaveCar(car);
+
+    this.setState({
+      newCarMake: '',
+      newCarModel: '',
+      newCarYear: 0,
+      newCarColor: '',
+      newCarPrice: 0,
+    });
+  }
+
+  render() {
+
+    return <form>
+      <div>
+        <label htmlFor="new-car-make-input">Make:</label>
+        <input type="text" id="new-car-make-input" name="newCarMake"
+          value={this.state.newCarMake} onChange={this.onChange} />
+      </div >
+      <div>
+        <label htmlFor="new-car-model-input">Model:</label>
+        <input type="text" id="new-car-model-input" name="newCarModel"
+          value={this.state.newCarModel} onChange={this.onChange} />
+      </div >
+      <div>
+        <label htmlFor="new-car-year-input">Year:</label>
+        <input type="number" id="new-car-year-input" name="newCarYear"
+          value={this.state.newCarYear} onChange={this.onChange} />
+      </div >
+      <div>
+        <label htmlFor="new-car-color-input">Color:</label>
+        <input type="text" id="new-car-color-input" name="newCarColor"
+          value={this.state.newCarColor} onChange={this.onChange} />
+      </div >
+      <div>
+        <label htmlFor="new-car-price-input">Price:</label>
+        <input type="number" id="new-car-price-input" name="newCarPrice"
+          value={this.state.newCarPrice} onChange={this.onChange} />
+      </div >
+      <button type="button" onClick={this.save}>Add Car</button>
+    </form>;
+  }
+}
--- client/js/components/car-tool.js
+++ client/js/components/car-tool.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { ToolHeader } from './tool-header';
 import { CarTable } from './car-table';
+import { CarForm } from './car-form';
 
 export class CarTool extends React.Component {
 
@@ -23,26 +24,13 @@ export class CarTool extends React.Component {
     });
   }
 
-  onClick = () => {
-    const nextId = Math.max(...this.state.cars.map(c => c.id)) + 1;
-    console.log(nextId);
-    const car = {
-      id: nextId,
-      make: this.state.newCarMake,
-      model: this.state.newCarModel,
-      year: this.state.newCarYear,
-      color: this.state.newCarColor,
-      price: this.state.newCarPrice,
-    };
+  saveCar = car => {
+    console.log("got here")
+    console.log(car);
 
     // add objects immutably; create a new array with concat
     this.setState({
       cars: this.state.cars.concat(car),
-      newCarMake: '',
-      newCarModel: '',
-      newCarYear: 0,
-      newCarColor: '',
-      newCarPrice: 0,
     });
   }
 
@@ -66,35 +54,7 @@ export class CarTool extends React.Component {
       <CarTable cars={this.state.cars} onDeleteCar={this.deleteCar}/>
       <br />
       <br />
-      <form>
-        <div>
-          <label htmlFor="new-car-make-input">Make:</label>
-          <input type="text" id="new-car-make-input" name="newCarMake"
-            value={this.state.newCarMake} onChange={this.onChange} />
-        </div >
-        <div>
-          <label htmlFor="new-car-model-input">Model:</label>
-          <input type="text" id="new-car-model-input" name="newCarModel"
-            value={this.state.newCarModel} onChange={this.onChange} />
-        </div >
-        <div>
-          <label htmlFor="new-car-year-input">Year:</label>
-          <input type="number" id="new-car-year-input" name="newCarYear"
-            value={this.state.newCarYear} onChange={this.onChange} />
-        </div >
-        <div>
-          <label htmlFor="new-car-color-input">Color:</label>
-          <input type="text" id="new-car-color-input" name="newCarColor"
-            value={this.state.newCarColor} onChange={this.onChange} />
-        </div >
-        <div>
-          <label htmlFor="new-car-price-input">Price:</label>
-          <input type="number" id="new-car-price-input" name="newCarPrice"
-            value={this.state.newCarPrice} onChange={this.onChange} />
-        </div >
-        <button type="button" onClick={this.onClick}>Add Car</button>
-      </form>
-
+      <CarForm cars={this.state.cars} onSaveCar={this.saveCar} />
     </div >;
   }
 
